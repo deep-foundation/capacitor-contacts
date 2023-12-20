45cf78838eceef9b9061f419e306f0b1b79aa0ec
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-20 16:00:56 +0600

    Export SaveAllContactsOptions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 
 export {checkPermissions} from './checkPermissions.js'
 export {requestPermissions} from './requestPermissions.js'
-export {saveAllContacts} from './saveAllContacts.js'
+export {saveAllContacts,SaveAllContactsOptions} from './saveAllContacts.js'
 
 
