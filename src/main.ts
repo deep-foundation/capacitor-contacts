a72328e1523c30dab46d20fdd4bd1e46b68dad97
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-20 16:06:42 +0600

    Export useContactsSync

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-
+export {UseContactsSyncOptions,useContactsSync} from './react/hooks/use-contacts-sync.js'
 export {checkPermissions} from './checkPermissions.js'
 export {requestPermissions} from './requestPermissions.js'
 export {CAPACITOR_CONTACT_PACKAGE_NAME, CAPACITOR_CONTACT_PACKAGE_NAME as NAME, CAPACITOR_CONTACT_PACKAGE_NAME as PACKAGE_NAME} from './name.js'
 export {saveAllContacts,SaveAllContactsOptions} from './saveAllContacts.js'
+ 
 
 
