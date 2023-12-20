18e38ae27272ac377da1072f98b178202c5b2b88
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-20 15:59:09 +0600

    Rename

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 
 export {checkPermissions} from './check-permissions.js'
-export {requestPermissions} from './request-permissions.js'
+export {requestPermissions} from './requestPermissions.js'
 
 
 
