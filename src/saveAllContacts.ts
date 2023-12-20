20d63d6ea8c1f9787bd2cbd9ff8bb8141ac31b39
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-20 15:59:05 +0600

    Move saveAllContacts to separate file

---
+++ src/saveAllContacts.ts
@@ -0,0 +1,78 @@
+import { Contacts } from '@capacitor-community/contacts';
+import { DeepClient, SerialOperation } from "@deep-foundation/deeplinks/imports/client.js";
+
+
+
+
+export async function saveAllContacts({ deep, containerLinkId }: { deep: DeepClient; containerLinkId: number; }) {
+  const contactTypeLinkId = await deep.id("@deep-foundation/capacitor-contact", "Contact");
+  const containTypeLinkId = await deep.id("@deep-foundation/core", "Contain");
+
+  const { contacts } = await Contacts.getContacts(
+    {
+      projection: {
+        name: true,
+        organization: true,
+        birthday: true,
+        note: true,
+        phones: true,
+        emails: true,
+        urls: true,
+        postalAddresses: true,
+        image: true,
+      }
+    });
+
+  const { data: existingContactLinks } = await deep.select({
+    type_id: contactTypeLinkId,
+    object: {
+      _or: contacts.map(contact => ({
+        value: {
+          _eq: contact.contactId
+        },
+        in: {
+          type_id: containTypeLinkId,
+          from_id: containerLinkId
+        }
+      }))
+    }
+  });
+  const operations: Array<SerialOperation> = [];
+  const updateOperations: SerialOperation[] = existingContactLinks.map(existingContactLink => {
+    const contact = contacts.find(contact => contact.contactId === existingContactLink.value!.value.contactId);
+    return {
+      type: "update",
+      table: "links",
+      exp: {
+        id: existingContactLink.id,
+      },
+      value: {
+        value: contact
+      }
+    };
+  });
+  operations.push(...updateOperations);
+  const newContacts = contacts.filter(contact => existingContactLinks.some(existingContactLink => existingContactLink.value!.value.contactId === contact.contactId));
+  const insertOperation: SerialOperation = {
+    type: 'insert',
+    table: 'links',
+    objects: newContacts.map((contact) => ({
+      type_id: contactTypeLinkId,
+      object: {
+        data: {
+          value: contact
+        }
+      },
+      in: {
+        data: {
+          type_id: containTypeLinkId,
+          from_id: containerLinkId
+        }
+      }
+    }))
+  };
+  operations.push(insertOperation);
+  return await deep.serial({
+    operations
+  });
+}
