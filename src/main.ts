9fc76d20dd9536dac96ba3d7f71a9e30371912e3
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-20 16:01:31 +0600

    Export CAPACITOR_CONTACT_PACKAGE_NAME

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 
 export {checkPermissions} from './checkPermissions.js'
 export {requestPermissions} from './requestPermissions.js'
+export {CAPACITOR_CONTACT_PACKAGE_NAME, CAPACITOR_CONTACT_PACKAGE_NAME as NAME, CAPACITOR_CONTACT_PACKAGE_NAME as PACKAGE_NAME} from './name.js'
 export {saveAllContacts,SaveAllContactsOptions} from './saveAllContacts.js'
 
 
