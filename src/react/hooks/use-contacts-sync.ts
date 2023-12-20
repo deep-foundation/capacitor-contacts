3a74ae3892f132a2e2649ab6d39c42f620684264
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-20 16:06:17 +0600

    Add useContactsSync

---
+++ src/react/hooks/use-contacts-sync.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react"
+import { saveAllContacts } from "../../saveAllContacts.js"
+import { DeepClient } from "@deep-foundation/deeplinks/imports/client.js"
+
+export function useContactsSync(options: UseContactsSyncOptions) {
+  const {intervalInMs = 60*1000, deep, containerLinkId} = options
+  useEffect(() => {
+    const interval = setInterval(() => {
+      saveAllContacts({deep, containerLinkId})
+    }, intervalInMs)
+    return () => {
+      clearInterval(interval)
+    }
+  })
+}
+
+export interface UseContactsSyncOptions {
+  deep: DeepClient;
+  containerLinkId: number;
+  intervalInMs?: number
+}
