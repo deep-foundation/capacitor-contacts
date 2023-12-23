b91a59c7fea8f2fad81e992cb3c135794cdb61e2
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-23 14:46:58 +0600

    Export usePermissionsStatus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 export {WithContactsSync,WithContactsSyncOptions} from './react/components/with-contacts-sync.js'
+export {UsePermissionsStatusResult,usePermissionsStatus} from './react/hooks/use-contacts-permissions.js'
 export {UseContactsSyncOptions,useContactsSync} from './react/hooks/use-contacts-sync.js'
 export {checkPermissions} from './checkPermissions.js'
 export {requestPermissions} from './requestPermissions.js'
