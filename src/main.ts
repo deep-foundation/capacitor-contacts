8cbff523bcdf38c6fd705e807ffc2121e2d467c9
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-23 14:50:22 +0600

    Add aliases

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 export {WithContactsSync,WithContactsSyncOptions} from './react/components/with-contacts-sync.js'
-export {UsePermissionsStatusResult,usePermissionsStatus} from './react/hooks/use-contacts-permissions.js'
+export {UsePermissionsStatusResult, UsePermissionsStatusResult as  UseContactsPermissionsStatusResult,usePermissionsStatus, usePermissionsStatus as useContactsPermissionsStatus} from './react/hooks/use-contacts-permissions.js'
 export {UseContactsSyncOptions,useContactsSync} from './react/hooks/use-contacts-sync.js'
-export {checkPermissions} from './checkPermissions.js'
-export {requestPermissions} from './requestPermissions.js'
+export {checkPermissions, checkPermissions as checkConctactsPermissions} from './checkPermissions.js'
+export {requestPermissions, requestPermissions as requestContactsPermissions} from './requestPermissions.js'
 export {CAPACITOR_CONTACT_PACKAGE_NAME, CAPACITOR_CONTACT_PACKAGE_NAME as NAME, CAPACITOR_CONTACT_PACKAGE_NAME as PACKAGE_NAME} from './name.js'
 export {saveAllContacts,SaveAllContactsOptions} from './saveAllContacts.js'
  
